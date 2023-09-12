09e3750f295596d1c13a1509413183bede8a2742
JESLITAVIYOLAALMEIDA <77922477+JESLITAVIYOLAALMEIDA@users.noreply.github.com>
2023-09-12 13:33:09 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -5,25 +5,22 @@ public class Main {
     public int healthy = 0;
     public int exchange = 0;
     public int failed = 0;
-  };
+  }
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
     for (int presentCapacity : presentCapacities) {
-            int ratedCapacity = 120;
-            // Calculate SoH for each battery
-            double soh = (presentCapacity / (double) ratedCapacity) * 100;
-            if (soh > 80) {
-                counts.healthy++;
-            } 
-            else if ( soh > 63) {
-                    counts.exchange++;
-            } 
-            else {
-            counts.failed++;
-        }
-
-        }
+      int ratedCapacity = 120;
+      // Calculate SoH for each battery
+      double soh = (presentCapacity / (double) ratedCapacity) * 100;
+      if (soh > 80) {
+        counts.healthy++;
+      } else if (soh > 63) {
+        counts.exchange++;
+      } else {
+        counts.failed++;
+      }
+    }
     return counts;
   }
 
@@ -31,9 +28,9 @@ public class Main {
     System.out.println("Counting batteries by SoH...\n");
     int[] presentCapacities = {115, 118, 80, 95, 91, 72};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
-    assert(counts.healthy == 2);
-    assert(counts.exchange == 3);
-    assert(counts.failed == 1);
+    assert counts.healthy == 2;
+    assert counts.exchange == 3;
+    assert counts.failed == 1;
     
     System.out.println("Done counting :)\n");
     System.out.println("All the test cases are passed :)\n"); 
